b7507bb52c9861a48a508347c0c4fc3bebef610f
Marjan Ahmed <marjan.ahmed08@yahoo.com>
2025-10-01 09:38:28 +0500

    chore: comment out global type declarations for '@splinetool/react-spline'

--- frontend/global.d.ts
+++ frontend/global.d.ts
@@ -1,20 +1,20 @@
-// Global type declarations
-declare module '@splinetool/react-spline' {
-  import { ComponentType } from 'react';
+// // Global type declarations
+// declare module '@splinetool/react-spline' {
+//   import { ComponentType } from 'react';
 
-  interface SplineProps {
-    scene: string;
-    onLoad?: (splineApp: any) => void;
-    onSplineMouseDown?: (e: any) => void;
-    onSplineMouseUp?: (e: any) => void;
-    onSplineMouseHover?: (e: any) => void;
-    onSplineKeyDown?: (e: any) => void;
-    onSplineKeyUp?: (e: any) => void;
-    onSplineStart?: (e: any) => void;
-    className?: string;
-    style?: React.CSSProperties;
-  }
+//   interface SplineProps {
+//     scene: string;
+//     onLoad?: (splineApp: any) => void;
+//     onSplineMouseDown?: (e: any) => void;
+//     onSplineMouseUp?: (e: any) => void;
+//     onSplineMouseHover?: (e: any) => void;
+//     onSplineKeyDown?: (e: any) => void;
+//     onSplineKeyUp?: (e: any) => void;
+//     onSplineStart?: (e: any) => void;
+//     className?: string;
+//     style?: React.CSSProperties;
+//   }
 
-  const Spline: ComponentType<SplineProps>;
-  export default Spline;
-}
+//   const Spline: ComponentType<SplineProps>;
+//   export default Spline;
+// }
--- frontend/types/spline.d.ts
+++ frontend/types/spline.d.ts
@@ -1,19 +1,19 @@
-declare module '@splinetool/react-spline' {
-  import { ComponentType } from 'react';
+// declare module '@splinetool/react-spline' {
+//   import { ComponentType } from 'react';
 
-  interface SplineProps {
-    scene: string;
-    onLoad?: (splineApp: any) => void;
-    onSplineMouseDown?: (e: any) => void;
-    onSplineMouseUp?: (e: any) => void;
-    onSplineMouseHover?: (e: any) => void;
-    onSplineKeyDown?: (e: any) => void;
-    onSplineKeyUp?: (e: any) => void;
-    onSplineStart?: (e: any) => void;
-    className?: string;
-    style?: React.CSSProperties;
-  }
+//   interface SplineProps {
+//     scene: string;
+//     onLoad?: (splineApp: any) => void;
+//     onSplineMouseDown?: (e: any) => void;
+//     onSplineMouseUp?: (e: any) => void;
+//     onSplineMouseHover?: (e: any) => void;
+//     onSplineKeyDown?: (e: any) => void;
+//     onSplineKeyUp?: (e: any) => void;
+//     onSplineStart?: (e: any) => void;
+//     className?: string;
+//     style?: React.CSSProperties;
+//   }
 
-  const Spline: ComponentType<SplineProps>;
-  export default Spline;
-}
+//   const Spline: ComponentType<SplineProps>;
+//   export default Spline;
+// }
